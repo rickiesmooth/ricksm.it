2f8f3ca75358a1c4755b499ed09b43daab2b8af1
Rick Smit <rick.p.smit@gmail.com>
2020-05-04 10:09:05 +0200

    added github actions

--- content/pages/index.md
+++ content/pages/index.md
@@ -3,6 +3,39 @@ title: Home
 description: All about Rick Smit
 ---
 
-# Hi! I'm Rick 👋
+# Rick Smit
 
-I'm a developer {.style-me}
+## Javascript/Node developer
+
+### Experience
+
+More than 5 years experience as a developer in various roles.
+
+- WeTransfer
+  - Frontend Developer | Feb 2020 - present
+  - React, Typescript, GraphQL, Redux, Kubernetes, Docker
+- Ahold
+  - Frontend Developer | Apr 2018 - Oct 2019
+  - React, Typescript, GraphQL, Redux, Kubernetes, Docker
+- Vigour
+  - Fullstack Developer | Apr 2014 - May 2018
+  - Vanilla JS, Socket-io, Browserify, ES6, React
+- Google
+  - Business Intern LCS | Apr 2013 - Sep 2013
+  - DFP, Google Apps, MySQL, GTM
+
+### Hobby Projects
+
+More than 5 years experience as a developer in various roles.
+
+- WeTransfer
+- Ahold
+- Google
+
+### Open Source
+
+More than 5 years experience as a developer in various roles.
+
+- WeTransfer
+- Ahold
+- Google
--- webpack.config.js
+++ webpack.config.js
@@ -3,29 +3,31 @@ const path = require('path')
 const fs = require('fs').promises
 const fm = require('front-matter')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
-const markdownItAttrs = require('markdown-it-attrs')
-const md = require('markdown-it')().use(markdownItAttrs)
+
+const md = require('markdown-it')().use(require('markdown-it-attrs'))
+
+function generatePages([pathname, file]) {
+  const {
+    attributes: { title, description, slug },
+    body,
+  } = fm(file)
+  const filename = path.basename(pathname, path.extname(pathname))
+
+  return new HtmlWebPackPlugin({
+    template: 'src/html/template.html',
+    filename: (slug || filename) + '.html',
+    templateParameters() {
+      return { body: md.render(body), title, description }
+    },
+  })
+}
 
 module.exports = async (env, argv) => {
   const content = glob.sync('content/pages/*.md')
   const html = await Promise.all(
     content.map(async (path) => [path, await fs.readFile(path, 'utf-8')])
   )
-  const pages = html.map(([filename, file]) => {
-    const {
-      attributes: { title, description, slug },
-      body,
-    } = fm(file)
-
-    return new HtmlWebPackPlugin({
-      template: 'src/html/template.html',
-      filename:
-        (slug || path.basename(filename, path.extname(filename))) + '.html',
-      templateParameters() {
-        return { body: md.render(body), title, description }
-      },
-    })
-  })
+  const pages = html.map(generatePages)
 
   return {
     entry: {
