56d5e60862ba49e41a5ba7edea9e69f46790887a
Rick Smit <rick.p.smit@gmail.com>
2020-06-07 15:57:07 +0200

    inline scripts

---
+++ src/html/template.js
@@ -0,0 +1,20 @@
+module.exports = (templateData) => {
+  const { htmlWebpackPlugin } = templateData
+  if (htmlWebpackPlugin.options.templateParameters.partial) {
+    return `${htmlWebpackPlugin.options.templateParameters.body}
+    ${htmlWebpackPlugin.files.js.map(
+      (jsFile) => `<script>
+      ${templateData.compilation.assets[
+        jsFile.substr(htmlWebpackPlugin.files.publicPath.length)
+      ].source()}</script>`
+    )}
+    `
+  }
+  return `
+    ${require('./shell-start.ejs')(templateData)}
+    <div id="content" class="md-container max-w-screen-md container mx-auto px-4 my-16">
+      ${htmlWebpackPlugin.options.templateParameters.body}
+    </div>
+    ${require('./shell-end.ejs')(templateData)}
+  `
+}
