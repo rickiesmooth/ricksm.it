ba0cb887a6f2c2090464675e58bfe58d86472da8
Rick Smit <rick.p.smit@gmail.com>
2020-08-28 10:22:12 +0200

    run cypress locally

--- cypress/integration/smoke.spec.js
+++ cypress/integration/smoke.spec.js
@@ -14,13 +14,26 @@ describe('Smoke Test', () => {
     cy.route({
       method: 'GET',
       url: '*/github',
+      response: {
+        showcaseItems: [
+          {
+            name: 'home',
+            url: 'https://github.com/rickiesmooth/home',
+            descriptionHTML:
+              '<div>React UI for <a href="https://github.com/mozilla-iot/gateway">https: //github.com/mozilla-iot/gateway</a>. </div>',
+            repositoryTopics: {
+              edges: [{ node: { topic: { name: 'graphql' } } }],
+            },
+          },
+        ],
+      },
     }).as('apiRequest')
 
     cy.get('nav .ml-auto').contains('work').click()
 
     cy.wait('@workRequest')
     cy.get('#projects')
-    cy.wait('@apiRequest')
+    // cy.wait('@apiRequest')
     cy.get('#projects > li')
   })
 })
