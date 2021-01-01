f3bcb156766a6993b1c7d2ec27017529a3be6d1e
Rick Smit <rick.p.smit@gmail.com>
2021-01-01 16:24:38 +0100

    use prismjs instead of highlightjs

--- src/blog.ts
+++ src/blog.ts
@@ -1,12 +1,3 @@
-/// <reference path="../node_modules/highlight.js/types/index.d.ts" />
+import Prism from 'prismjs'
 
-import hljs from 'highlight.js/lib/core'
-import javascript from 'highlight.js/lib/languages/javascript'
-import typescript from 'highlight.js/lib/languages/typescript'
-
-import 'highlight.js/styles/github-gist.css'
-
-hljs.registerLanguage('javascript', javascript)
-hljs.registerLanguage('typescript', typescript)
-
-hljs.initHighlightingOnLoad()
+Prism.highlightAll()
