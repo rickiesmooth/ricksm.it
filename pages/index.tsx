608387fbffa51ebfe3b493f330da4013958d4f82
Rick Smit <rick.p.smit@gmail.com>
2021-12-04 15:01:05 +0100

    add privacy page

--- pages/index.tsx
+++ pages/index.tsx
@@ -81,7 +81,7 @@ export default function Index() {
           </Link>
         </div>
       </div>
-      <p className="text-center">fingerprint: 51D5 6BB0 17A3 2B9F BB17  84F6 3DEF 4FED D177 C6FB </p>
+      <p className="text-center text-xs">fingerprint: 51D5 6BB0 17A3 2B9F BB17  84F6 3DEF 4FED D177 C6FB </p>
     </Layout>
   )
 }
