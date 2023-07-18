5c89c77a9d8d1aa8d4e66dafa1c8fe2557f3d8a6
Rick Smit <rick.p.smit@gmail.com>
2023-07-18 15:26:31 +0200

    deleting old files before adding updated project

--- components/Link.tsx
+++
@@ -1,9 +0,0 @@
-import { default as NextLink, LinkProps } from 'next/link'
-
-export function Link({ as, href, ...otherProps }: React.PropsWithChildren<LinkProps>) {
-  return (
-    <NextLink as={as} href={href}>
-      <a className="text-blue-400" {...otherProps} />
-    </NextLink>
-  )
-}
--- vendor/webgl-utils.d.ts
+++
@@ -1,6 +0,0 @@
-export function create3DContext(canvas: HTMLElement, opt_attribs?: any): any
-
-export function setupWebGL(
-  canvas: HTMLElement,
-  opt_attribs?: any
-): WebGLRenderingContext
